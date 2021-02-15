5f4a356a5242d2961da5124e11597b3591e26fe7
maggness <timderoller@gmail.com>
2021-02-15 16:32:52 +0100

    test

---
+++ README.md
@@ -0,0 +1,8 @@
+# Welcome to the matching app of Tim de Roller
+I am a student at the University of Applied Sciences Amsterdam and this is my matching app for Project Tech.
+
+# Table of Contents
+
+# About this App
+
+# The last things
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 5181
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("static"));
+
+app.get('/', function(req, res) {
+  res.render('pages/index.ejs');
+});
+
+app.get('/about', function(req, res) {
+  res.render('pages/about.ejs');
+});
+
+app.get('/test', (req, res) => {
+res.send('test swag')
+})
+
+app.listen(port, () => {
+    console.log("Server opgestart at http://localhost:")
+})
