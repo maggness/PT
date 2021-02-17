c1c0a2aca79121b9c24d69503317844f0a37806a
maggness <timderoller@gmail.com>
2021-02-17 14:54:09 +0100

    versie 1.0.2

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ app.use(express.static("static"));
 
 app.get('/', function(req, res) {
   res.render('pages/index.ejs');
+
+  var games = [
+      { id: '1', name: 'CSGO', genre: "FPS"},
+      { id: '2', name: 'Destiny', genre: "FPS MMO"},
+      { id: '3', name: 'Minecraft', genre: "Survival"}
+  ];
+
+  res.render('pages/index', {
+      games: games
+  });
+
+});
+
+app.get("/", function (req, res){
+    var spel = req.query.spel; // Hier door staat het in de URL
 });
 
 app.get('/about', function(req, res) {
@@ -18,9 +33,8 @@ app.get('/test', (req, res) => {
 res.send('test swag')
 })
 
-app.listen(port, () => {
-    console.log("Server opgestart at http://localhost:")
-})
+app.listen(port, () => console.log(`Server opgestart at http://localhost:${port}`))
 
-app.use(function(req, res, next){
-  res.status(404);
+app.use(function (req, res, next) {
+  res.status(404).send("Error 404")
+});
