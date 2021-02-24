12a8d3ed9addf857d9d3b2c6bd79cc597a3dd108
maggness <timderoller@gmail.com>
2021-02-24 12:32:53 +0100

    Main test

--- server.js
+++ server.js
@@ -1,28 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 5181
-var mongo = require('mongodb');
-var db = null
-var url = 'mongodb+srv://' + process.env.DB_HOST + ':' + process.env.DB_PORT
-
-require('dotenv').config()
-
-mongo.MongoClient.connect(url, function (err, client) {
-  if (err) throw err
-  db = client.db(process.env.DB_NAME)
-})
-
-function movies(req, res, next) {
-  db.collection('movie').find().toArray(done)
-
-  function done(err, data) {
-    if (err) {
-      next(err)
-    } else {
-      res.render('list.ejs', {data: data})
-    }
-  }
-}
 
 app.set('view engine', 'ejs');
 
