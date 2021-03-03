3d65993eb6bdba790e326087094045f6e710da24
maggness <timderoller@gmail.com>
2021-03-03 13:07:48 +0100

    Versie 1.1.0

--- server.js
+++ server.js
@@ -1,40 +1,62 @@
+require("dotenv").config();
 const express = require('express');
+const bodyParser = require("body-parser")
+const path = require("path");
+const MongoClient = require('mongodb').MongoClient
+const port = 5181;
 const app = express();
-const port = 5181
 
-app.set('view engine', 'ejs');
+const { MONGO_USER, MONGO_PASS, MONGO_URI, MONGO_DB } = process.env;
 
-app.use(express.static("static"));
+main();
 
-app.get('/', function(req, res) {
-  res.render('pages/index.ejs');
+function main() {
+  MongoClient
+    .connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_URI}/${MONGO_DB}?retryWrites=true&w=majority`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
 
-  var games = [
-      { id: '1', name: 'CSGO', genre: "FPS"},
-      { id: '2', name: 'Destiny', genre: "FPS MMO"},
-      { id: '3', name: 'Minecraft', genre: "Survival"}
-  ];
+    .then(connection => {
 
-  res.render('pages/index', {
-      games: games
-  });
+      const test = "swag";
+      const db = connection.db('ProjectTech')
+      const genreCollection = db.collection('Genre')
 
-});
+      app.set("view engine", "ejs");
+      app.set("views", path.join(__dirname, "views"));
+      app.use(bodyParser.urlencoded({extended: true}))
+      app.use(express.static('static'))
 
-app.get("/", function (req, res){
-    var spel = req.query.spel; // Hier door staat het in de URL
-});
+      app.get('/', (req, res) => {
+        res.render('pages/index.ejs')
+      })
 
-app.get('/about', function(req, res) {
-  res.render('pages/about.ejs');
-});
+      app.get('/liked', (req, res) => {
+          res.render('pages/liked.ejs')
+        })
 
-app.get('/test', (req, res) => {
-res.send('test swag')
-})
+      app.post('/genre', (req, res) => {
+        genreCollection.insertOne(req.body)
+          .then(result => {
+            res.redirect('/')
+          })
+          .catch(error => console.error(error))
+      })
 
-app.listen(port, () => console.log(`Server opgestart at http://localhost:${port}`))
+      // app.get('/', (req, res) => {
+      //   db.collection('Genre').find().toArray()
+      //     .then(quotes => {
+      //       res.render('index.ejs', { Genre: name })
+      //     })
+      //     .catch(/* ... */)
+      // })
 
-app.use(function (req, res, next) {
-  res.status(404).send("Error 404")
-});
+      app.use(function (req, res, next) {
+        res.status(404).send("Error 404")
+      });
+
+      app.listen(port, () => console.log(`Yo ${MONGO_USER}, de server is opgestart bij http://localhost:${port}`))
+
+    });
+}
