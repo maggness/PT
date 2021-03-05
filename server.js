e21d15634d0057eb25d4fcf0d106ad9d71989521
maggness <timderoller@gmail.com>
2021-03-05 12:07:44 +0100

    Versie 1.2.0 | Database Post & Get

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 require("dotenv").config();
 const express = require('express');
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser") // bodyParser zorgt ervoor dat ik data kan halen uit mijn body
 const path = require("path");
 const MongoClient = require('mongodb').MongoClient
 const port = 5181;
 const app = express();
 
+// Haalt de dingen uit mijn .env bestand
 const { MONGO_USER, MONGO_PASS, MONGO_URI, MONGO_DB } = process.env;
 
+// Main function aan roepen
 main();
 
+let hetSpel = {
+  name: "", genre:""
+}
+
+// Connectie maken met de database
 function main() {
   MongoClient
     .connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_URI}/${MONGO_DB}?retryWrites=true&w=majority`, {
@@ -17,12 +24,14 @@ function main() {
       useUnifiedTopology: true,
     })
 
+    // Promise, als de connectie er is voert het deze dingen uit
     .then(connection => {
 
-      const test = "swag";
+      const test = "<p>swag</p>";
       const db = connection.db('ProjectTech')
-      const genreCollection = db.collection('Genre')
+      const genreCollection = db.collection('game')
 
+      // Het aanroepen van mijn gebruikte engine en dan het pad van mijn views
       app.set("view engine", "ejs");
       app.set("views", path.join(__dirname, "views"));
       app.use(bodyParser.urlencoded({extended: true}))
@@ -32,11 +41,7 @@ function main() {
         res.render('pages/index.ejs')
       })
 
-      app.get('/liked', (req, res) => {
-          res.render('pages/liked.ejs')
-        })
-
-      app.post('/genre', (req, res) => {
+      app.post('/spel', (req, res) => {
         genreCollection.insertOne(req.body)
           .then(result => {
             res.redirect('/')
@@ -44,18 +49,35 @@ function main() {
           .catch(error => console.error(error))
       })
 
-      // app.get('/', (req, res) => {
-      //   db.collection('Genre').find().toArray()
-      //     .then(quotes => {
-      //       res.render('index.ejs', { Genre: name })
-      //     })
-      //     .catch(/* ... */)
-      // })
+      app.get('/', (req, res) => {
+        res.render('pages/index.ejs', {spel:hetSpel})
+      })
 
+      app.get('/profiel', (req, res) => {
+        db.collection('game').find().toArray()
+        .then(results => {
+          res.render('pages/profiel.ejs', {
+            spel: results,
+            genre: hetSpel,
+          })
+        })
+      })
+
+      app.get('/profiel', (req, res) => {
+      db.collection('game').find().toArray()
+        .then(results => {
+          console.log(results)
+        })
+        .catch(error => console.error(error))
+      // ...
+    })
+
+      // Als er een error is laat hij een error zien
       app.use(function (req, res, next) {
         res.status(404).send("Error 404")
       });
 
+      // Als mijn server is opgestart zie ik een bericht in mijn console
       app.listen(port, () => console.log(`Yo ${MONGO_USER}, de server is opgestart bij http://localhost:${port}`))
 
     });
